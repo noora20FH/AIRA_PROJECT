d163610d3c1bb2148b396993a728646f8a64b3fd
noora20FH <noora20fairy@gmail.com>
2022-06-06 20:12:08 +0700

    Admin Middleware

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->tinyInteger('role_as')->default('0'); //added in UserTable before timestamp
             $table->rememberToken();
             $table->timestamps();
         });
