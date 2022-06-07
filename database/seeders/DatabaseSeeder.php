0f4a8b5ec5dca11e83b2a0daf8a492ea0719b494
noora20FH <noora20fairy@gmail.com>
2022-06-07 11:37:34 +0700

    Admin seeder data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,23 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        
+        DB::table('users')->insert([
+            
+            'name' => 'Nian Gita',
+            'email'=> 'nian@gmail.com',
+            'password'=> Hash::make('niangita123'),
+            'role_as'=> 1,
+
+        ]);
+        DB::table('users')->insert([
+            
+            'name' => 'Dimas',
+            'email'=> 'dimas@gmail.com',
+            'password'=> Hash::make('dimas123'),
+            'role_as'=> 1,
+
+        ]);
+
     }
 }
