e7e7f5be8cf65a75ab6685559bd0f348ff0d0ce1
noora20FH <noora20fairy@gmail.com>
2022-06-14 22:30:15 +0700

    revisi Admin Panel

--- resources/views/admin/category/add.blade.php
+++ resources/views/admin/category/add.blade.php
@@ -4,22 +4,23 @@
     <div class="card">
         <div class="card-header">
             <h4>Add Category</h4>
-            </div>
+        </div>
             <div class="card-body">
-                <form action="{{ url('insert-category') }}" method="POST"> 
-                @csrf 
-                <div class="row">
-                        <div class="col-md-6">
+                <form action="{{ url('insert-category') }}" method="POST" enctype="multipart/form-data"> 
+                    @csrf 
+                    <div class="row">
+                        <div class="col-md-6 mb-3">
                             <label for="">Name</label>
                             <input type="text" class="form-control" name="name">
                         </div>
 
-                        <div class="col-md-6">
+                        <div class="col-md-6 mb-3">
                             <label for="">Slug</label>
                             <input type="text" class="form-control" name="slug">
                         </div>
 
-                        <div class="col-md-12">
+                        <div class="col-md-12 mb-3">
+                            <label for="">Description</label>
                             <textarea name="description" rows="3" class="form-control"></textarea>
                         </div>
 
@@ -48,14 +49,15 @@
                             <textarea name="meta_keywords" rows="3" class="form-control"></textarea>
                         </div>
 
-                        <div class="col-md-12">
+                        <div class="col-md-12 ">
                             <input type="file" name="image" class="form-control">
                         </div>
 
                         <div class="col-md-12">
                             <button type="submit" class="btn btn-primary">Submit</button>
                         </div>
+                    </div>   
                 </form>
-        </div>
+            </div>
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\WelcomeController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-
 
 Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
 Route::get('/shop', [WelcomeController::class, 'shop'])->name('shop');
@@ -33,7 +21,7 @@ Route::get('/home', [App\Http\Controllers\WelcomeController::class, 'index'])->n
 
 Route::middleware(['auth','isAdmin'])->group(function(){
     Route::get('/dashboard', 'Admin\FrontendController@index');
-                                //without calling class name
+                                //without calling class name -> 'use App\Http\....'
     Route::get('categories', 'Admin\CategoryController@index');
     Route::get('add-category','Admin\CategoryController@add');
     Route::post('insert-category','Admin\CategoryController@insert');
