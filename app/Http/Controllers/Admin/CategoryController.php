f8c717da50bf42c88aa830f3f98d3a21f3010760
noora20FH <noora20fairy@gmail.com>
2022-06-14 22:32:57 +0700

    revisi Admin Panel

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -32,8 +32,8 @@ class CategoryController extends Controller
        $category->name = $request->input('name');
        $category->slug = $request->input('slug');
        $category->description = $request->input('description');
-       $category->status = $request->input('status' == TRUE ? '1':'0');
-       $category->popular = $request->input('popular' == TRUE ? '1':'0');
+       $category->status = $request->input('status') == TRUE ? '1':'0';
+       $category->popular = $request->input('popular') == TRUE ? '1':'0';
        $category->meta_title = $request->input('meta_title');
        $category->meta_descrip = $request->input('meta_description');
        $category->meta_keywords = $request->input('meta_keywords');
